4d511bda731494036e7e102e1b09211cb7a46241
Rafael Dias <67400064+rafael-bit@users.noreply.github.com>
2024-09-26 08:56:58 -0300

    Add swagger

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const connectDB = require('./config/db');
 const userRoutes = require('./routes/user');
 const transactionRoutes = require('./routes/transactions');
 
+const swaggerUi = require('swagger-ui-express');
+const swaggerFile = require('./config/swagger_output.json');
+
 require('dotenv').config();
 
 const app = express();
@@ -13,6 +16,7 @@ app.use(cors());
 app.use(express.json());
 app.use('/api/users', userRoutes);
 app.use('/api/transactions', transactionRoutes);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use((err, req, res, next) => {
 	res.status(500).json({ message: err.message });
---
+++ config/swagger.js
@@ -0,0 +1,17 @@
+const swaggerAutogen = require('swagger-autogen')();
+
+const doc = {
+	info: {
+		title: 'API de Exemplo',
+		description: 'Descrição da API',
+	},
+	host: 'localhost:8080',
+	schemes: ['http'],
+};
+
+const outputFile = './config/swagger_output.json';
+const endpointsFiles = ['../app.js'];
+
+swaggerAutogen(outputFile, endpointsFiles).then(() => {
+	require('../app');
+});
